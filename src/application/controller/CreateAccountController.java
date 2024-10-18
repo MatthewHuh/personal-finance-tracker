4b611a091f75ddd59758c43d82e6ce5333b63dcd
Matthew Huh <huhdmatthew@gmail.com>
2024-10-18 02:47:44 -0700

    added error labels to Create Account page

--- src/application/controller/CreateAccountController.java
+++ src/application/controller/CreateAccountController.java
@@ -3,15 +3,25 @@ package application.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class CreateAccountController {
 
     @FXML
+    private Label accountNameErrorMsg;
+
+    @FXML
     private TextField accountNameText;
 
     @FXML
+    private Label openingBalanceErrorMsg;
+
+    @FXML
     private TextField openingBalanceText;
+
+    @FXML
+    private Label openingDateErrorMsg;
 
     @FXML
     private DatePicker openingDatePicker;
