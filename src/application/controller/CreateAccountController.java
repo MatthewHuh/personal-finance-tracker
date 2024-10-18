61a545146a93b3904f263459be153e3abeb40fe8
Matthew Huh <huhdmatthew@gmail.com>
2024-10-18 02:14:23 -0700

    Designed Create Account page added skeleton to CreateAccountController.java. Page still needs to be programmed. Changed starting page to Create Account Page to text

--- src/application/controller/CreateAccountController.java
+++ src/application/controller/CreateAccountController.java
@@ -1,5 +1,34 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class CreateAccountController {
+
+    @FXML
+    private TextField accountNameText;
+
+    @FXML
+    private TextField openingBalanceText;
+
+    @FXML
+    private DatePicker openingDatePicker;
+
+    @FXML
+    void onCancelAction(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void onDateAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSubmitAction(ActionEvent event) {
+
+    }
 
 }
