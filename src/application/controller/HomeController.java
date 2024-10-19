4b3e8a9e98d6b2a893a6ca7e4228be8f09daf1d2
Gio Hsieh <giovanni.hsieh@sjsu.edu>
2024-10-19 14:21:44 -0700

    added homepage in Home.fxml Edited Main to display homepage instead of create account page

--- src/application/controller/HomeController.java
+++ src/application/controller/HomeController.java
@@ -1,5 +1,57 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class HomeController {
+
+    @FXML
+    private Button createNewAccountButton;
+
+    @FXML
+    private Button defineNewTransactionButton;
+
+    @FXML
+    private Button enterTransactionButton;
+
+    @FXML
+    private Button scheduleTransactionButton;
+
+    @FXML
+    private Button searchTransactionButton;
+
+    @FXML
+    private Button viewReportsButton;
+
+    @FXML
+    void onCreateNewAccount(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onDefineNewTransaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEnterTransaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onScheduleTransaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSearchTransaction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onViewReports(ActionEvent event) {
+
+    }
 
 }
