87f9ca726a1ad175108171eec5f5e05db1a21dd9
Matthew Huh <huhdmatthew@gmail.com>
2024-10-31 00:52:10 -0700

    created new classes for transations and transaction types

---
+++ src/application/TransactionType.java
@@ -0,0 +1,18 @@
+package application;
+
+public class TransactionType {
+
+	private String transactionType;
+	
+	public TransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	
+	public String getTransactionType() {
+		return transactionType;
+	}
+	
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+}
---
+++ src/application/controller/DefineTransactionTypeController.java
@@ -0,0 +1,30 @@
+package application.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class DefineTransactionTypeController {
+
+    @FXML
+    private BorderPane createAccountPane;
+
+    @FXML
+    private Label transactionTypeErrorMsg;
+
+    @FXML
+    private TextField transactionTypeText;
+
+    @FXML
+    void onCancelAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onSubmitAction(ActionEvent event) {
+
+    }
+
+}
