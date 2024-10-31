28364e318020cbf67ce25cb7ecef2f5590e70ce2
Matthew Huh <huhdmatthew@gmail.com>
2024-10-31 12:48:04 -0700

    added basic navigation

--- src/application/controller/DefineTransactionTypeController.java
+++ src/application/controller/DefineTransactionTypeController.java
@@ -1,10 +1,16 @@
 package application.controller;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 public class DefineTransactionTypeController {
 
@@ -19,12 +25,40 @@ public class DefineTransactionTypeController {
 
     @FXML
     void onCancelAction(ActionEvent event) {
-
+    	try {
+    		// Load the Home.fxml file
+    		Parent homeView = FXMLLoader.load(getClass().getClassLoader().getResource("view/Home.fxml"));
+    		
+    		// Get the current stage
+			Stage stage = (Stage) createAccountPane.getScene().getWindow();
+			
+			// Set the new scene
+			stage.setScene(new Scene(homeView));
+			stage.setTitle("Home"); // Optional: Set the window title
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @FXML
     void onSubmitAction(ActionEvent event) {
-
+    	try {
+    		// Load the Home.fxml file
+    		Parent homeView = FXMLLoader.load(getClass().getClassLoader().getResource("view/Home.fxml"));
+    		
+    		// Get the current stage
+			Stage stage = (Stage) createAccountPane.getScene().getWindow();
+			
+			// Set the new scene
+			stage.setScene(new Scene(homeView));
+			stage.setTitle("Home"); // Optional: Set the window title
+			stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 }
