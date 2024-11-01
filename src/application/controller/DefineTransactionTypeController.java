a9dc6fd560519970d31bbb785ac200a9d4433405
Matthew Huh <huhdmatthew@gmail.com>
2024-11-01 13:22:11 -0700

    implemented creating new transaction type

--- src/application/controller/DefineTransactionTypeController.java
+++ src/application/controller/DefineTransactionTypeController.java
@@ -1,7 +1,11 @@
 package application.controller;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
+import application.TransactionType;
+import application.dao.AccountDAO;
+import application.dao.TransactionTypeDAO;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,6 +26,13 @@ public class DefineTransactionTypeController {
 
     @FXML
     private TextField transactionTypeText;
+    
+    private TransactionTypeDAO dao;
+    
+    public void initialize() {
+    	// initialize dao
+		dao = new TransactionTypeDAO();
+    }   
 
     @FXML
     void onCancelAction(ActionEvent event) {
@@ -44,21 +55,40 @@ public class DefineTransactionTypeController {
 
     @FXML
     void onSubmitAction(ActionEvent event) {
-    	try {
-    		// Load the Home.fxml file
-    		Parent homeView = FXMLLoader.load(getClass().getClassLoader().getResource("view/Home.fxml"));
-    		
-    		// Get the current stage
-			Stage stage = (Stage) createAccountPane.getScene().getWindow();
-			
-			// Set the new scene
-			stage.setScene(new Scene(homeView));
-			stage.setTitle("Home"); // Optional: Set the window title
-			stage.show();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    	boolean inputValidate = true;
+    	if(transactionTypeText.getText().equals("")) {
+    		transactionTypeErrorMsg.setText("Please enter the transaction type");
+    		inputValidate = false;
+    	}
+    	else if(dao.search(transactionTypeText.getText()) != null) {
+    		transactionTypeErrorMsg.setText("Account name taken. Please enter a unique name");
+    		inputValidate = false;
+    	}
+    	else {
+    		transactionTypeErrorMsg.setText("");
+    	}
+    	if(inputValidate) {
+	    	try {
+	    		// create new TransactionType object
+	    		TransactionType type = new TransactionType(transactionTypeText.getText());
+	    		
+	    		// write to db
+	    		dao.create(type);
+	    		
+	    		// Load the Home.fxml file
+	    		Parent homeView = FXMLLoader.load(getClass().getClassLoader().getResource("view/Home.fxml"));
+	    		
+	    		// Get the current stage
+				Stage stage = (Stage) createAccountPane.getScene().getWindow();
+				
+				// Set the new scene
+				stage.setScene(new Scene(homeView));
+				stage.setTitle("Home"); // Optional: Set the window title
+				stage.show();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }
     }
-
 }
